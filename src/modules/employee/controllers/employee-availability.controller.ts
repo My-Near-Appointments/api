8c5d555a54134e7feeb6b5f26a9d4d3ebe95a92c
Pedro Marins <ph.marins@hotmail.com>
2023-12-26 22:53:29 -0300

    feat: employee availability improvements

--- src/modules/employee/controllers/employee-availability.controller.ts
+++ src/modules/employee/controllers/employee-availability.controller.ts
@@ -1,31 +1,24 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   HttpStatus,
   Param,
   Post,
-  Put,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateEmployeeAvailabilityDto } from 'src/modules/employee/dtos/create-employee-availability.dto';
 import { EmployeeAvailabilityResponseDto } from 'src/modules/employee/dtos/employee-availability-response.dto';
-import { UpdateEmployeeAvailabilityDto } from 'src/modules/employee/dtos/update-employee-availability.dto';
 import { CreateEmployeeAvailabilityUseCase } from 'src/modules/employee/usecases/create-employee-availability.use-case';
-import { DeleteEmployeeAvailabilityUseCase } from 'src/modules/employee/usecases/delete-employee-availability.use-case';
 import { ListEmployeeAvailabilityUseCase } from 'src/modules/employee/usecases/list-employee-availability.use-case';
-import { UpdateEmployeeAvailabilityUseCase } from 'src/modules/employee/usecases/update-employee-availability.use-case';
 
 @Controller('v1/employee-availability')
 export class EmployeeAvailabilityController {
   constructor(
     private readonly createEmployeeAvailabilityUseCase: CreateEmployeeAvailabilityUseCase,
-    private readonly updateEmployeeAvailabilityUseCase: UpdateEmployeeAvailabilityUseCase,
     private readonly listEmployeeAvailabilityUseCase: ListEmployeeAvailabilityUseCase,
-    private readonly deleteEmployeeAvailabilityUseCase: DeleteEmployeeAvailabilityUseCase,
   ) {}
 
   @ApiTags('employee-availability')
@@ -35,8 +28,12 @@ export class EmployeeAvailabilityController {
     type: EmployeeAvailabilityResponseDto,
   })
   @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'Invalid or taken date range',
+  })
+  @ApiResponse({
     status: HttpStatus.BAD_REQUEST,
-    description: 'Validation errors',
+    description: 'Request error',
   })
   @UseGuards(AuthGuard('jwt'))
   @Post()
@@ -49,51 +46,20 @@ export class EmployeeAvailabilityController {
   @ApiTags('employee-availability')
   @ApiResponse({
     status: HttpStatus.OK,
-    description: 'Employee availability updated',
-    type: EmployeeAvailabilityResponseDto,
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Validation errors',
-  })
-  @UseGuards(AuthGuard('jwt'))
-  @Put(':employeeId')
-  async updateEmployeeAvailability(
-    @Param('employeeId') id: string,
-    @Body() data: UpdateEmployeeAvailabilityDto,
-  ) {
-    return await this.updateEmployeeAvailabilityUseCase.execute(id, data);
-  }
-
-  @ApiTags('employee-availability')
-  @ApiResponse({
-    status: HttpStatus.OK,
     description: 'Employee availability listed',
     type: EmployeeAvailabilityResponseDto,
   })
   @ApiResponse({
     status: HttpStatus.BAD_REQUEST,
-    description: 'Validation errors',
+    description: 'Request Error',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Server error',
   })
   @UseGuards(AuthGuard('jwt'))
   @Get(':employeeId')
   async listByEmployeeId(@Param('employeeId') id: string) {
     return await this.listEmployeeAvailabilityUseCase.execute(id);
-  }
-
-  @ApiTags('employee-availability')
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'Employee availability removed',
-    type: EmployeeAvailabilityResponseDto,
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Validation errors',
-  })
-  @UseGuards(AuthGuard('jwt'))
-  @Delete(':employeeId')
-  async deleteEmployeeAvailability(@Param('employeeId') id: string) {
-    return await this.deleteEmployeeAvailabilityUseCase.execute(id);
   }
 }
